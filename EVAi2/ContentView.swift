304eba8b08cf7a767e5d58132ef011d2a67f7385
TSC <siongchai@hotmail.com>
2026-06-14 14:38:23 +0800

    Initial Commit

---
+++ EVAi2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EVAi2
+//
+//  Created by TSC on 14/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EVAi2/EVAi2App.swift
@@ -0,0 +1,17 @@
+//
+//  EVAi2App.swift
+//  EVAi2
+//
+//  Created by TSC on 14/6/26.
+//
+
+import SwiftUI
+
+@main
+struct EVAi2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
